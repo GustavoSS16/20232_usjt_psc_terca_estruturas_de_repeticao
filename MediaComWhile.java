10349f4d0035f4799535a054cac6487045ee07e1
Gustavo Silva <gustavoss1994@gmail.com>
2023-09-12 21:48:25 -0300

    ap09(repeticao): media

---
+++ MediaComWhile.java
@@ -0,0 +1,19 @@
+public class MediaComWhile{
+    public static void main(String[] args) {
+        // declaracão de variáveis
+        short numeroAlunos, contador;
+        float nota1, nota2;
+
+        //entrada
+
+        //processamento
+
+        //saída
+
+
+    }
+
+
+
+
+}
